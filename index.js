4ec3b464510a0a101de69fe49427b2996da1cb6a
Steven Lantinga <steven@webmen.nl>
2022-03-08 09:54:08 +0100

    Clean up files and add to nav

--- index.js
+++ index.js
@@ -8,52 +8,56 @@ const PORT = 5000;
 const app = express();
 const pageRoutes = [
     {
-        id: 1,
         title: 'Introduction',
         route: '/',
         template: 'index.njk'
     },
     {
-        id: 2,
+        title: 'Nav',
+        route: '/nav',
+        template: 'nav.njk'
+    },
+    {
+        id: 3,
         title: 'Sections',
         route: '/sections',
         template: 'sections.njk'
     },
     {
-        id: 3,
         title: 'Visual',
         route: '/visual',
         template: 'visual.njk'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Content',
         route: '/content',
         template: 'content.njk'
     },
     {
-        id: 5,
         route: '/links',
         title: 'Links',
         template: 'links.njk'
     },
     {
-        id: 6,
         title: 'Tables',
         route: '/tables',
         template: 'tables.njk'
     },
     {
-        id: 7,
         title: 'Embed',
         route: '/embed',
         template: 'embed.njk'
     },
     {
-        id: 8,
         title: 'Holygrail',
         route: '/holygrail',
         template: 'holygrail.njk'
+    },
+    {
+        title: 'Messages',
+        route: '/messages',
+        template: 'messages.njk'
     }
 ];
 app.use(express.static(path.join(__dirname, '/')));
